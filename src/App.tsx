6735d0c8b2e2c94f5db1d37bc02c14f6cb6ae3b7
omerjaved65 <omer.oj6544@gmail.com>
2022-11-04 20:04:47 +0500

    package and files added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import HomePage from './Pages/Home';
+import { disconnect } from 'process';
+import Web3 from 'web3';
+import { injected } from './Constants/Injected';
+
+
+function getLibrary() {
+  const provider1 = new WalletConnectProvider({
+    rpc: {
+      97: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      1287: "https://rpc.api.moonbase.moonbeam.network",
+    },
+  });
+  return new Web3(provider1._provider)
+}
+function MetamaskProvider({ children }: { children: JSX.Element }): JSX.Element {
+  const { active: networkActive, error: networkError, activate: activateNetwork, deactivate } = useWeb3React()
+  const [loaded, setLoaded] = useState(false)
+  useEffect(() => {
+    injected
+      .isAuthorized()
+      .then((isAuthorized: any) => {
+        setLoaded(false)
+        if (localStorage.getItem('key') == 'true') {
+          activateNetwork(injected)
+        }
+        else if (localStorage.getItem('key') === 'false') {
+          disconnect()
+        }
+      })
+      .catch(() => {
+        setLoaded(false)
+
+      })
+  }, [activateNetwork, networkActive, networkError])
+
+  return <>
+    <HomePage />
+  </>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomePage />
     </div>
   );
 }
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <>
+            <div>
+                <button>
+                    Connect Wallet
+                </button>
+            </div>
+        </>
+    )
+}
+export default HomePage;
